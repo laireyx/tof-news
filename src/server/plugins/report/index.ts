5b2077b2dc193e920c98efce62833ab12259fae0
laireyx <laireyx@gmail.com>
2022-09-28 12:47:19 +0900

    Seperate media types

--- src/server/plugins/report/index.ts
+++ src/server/plugins/report/index.ts
@@ -1,10 +1,24 @@
 import fp from "fastify-plugin";
 
-type NewsMedia = {
-  type: "photo" | "animated_gif" | string;
-  url?: string | string[];
+type ImageMedia = {
+  type: "photo" | "animated_gif";
+  url?: string;
   previewUrl?: string;
 };
+
+type VideoMedia = {
+  type: "video";
+  url?: string[];
+  previewUrl?: string;
+};
+
+type MiscMedia = {
+  type: string;
+  url?: string | string[];
+  previewUro?: string;
+};
+
+type NewsMedia = ImageMedia | VideoMedia | MiscMedia;
 
 type News = {
   url: string;
