f1dc78f1fb2050cf3fc43b12300aa847d6e7affd
laireyx <laireyx@gmail.com>
2023-01-01 03:31:56 +0900

    Test commit

--- src/server/routes/nametag/index.ts
+++ src/server/routes/nametag/index.ts
@@ -7,9 +7,7 @@ export default async function (fastify: FastifyInstance) {
     {
       preValidation: (request, reply, done) => {
         const { uid } = request.params;
-        if (uid.length !== 17) {
-          done(new Error("Invalid UID"));
-        } else if (uid.match(/[^\d]/)) {
+        if (!uid.match(/[^\d]+(\.png)?/)) {
           done(new Error("Invalid UID"));
         }
 
@@ -17,7 +15,9 @@ export default async function (fastify: FastifyInstance) {
       },
     },
     async function (request, reply) {
-      const nametag = await fastify.tofNametag(request.params.uid);
+      const nametag = await fastify.tofNametag(
+        request.params.uid.replace(/\.png/, "")
+      );
       reply.header("Cache-Control", `max-age=${nametag.maxAge}`);
       reply.type("image/svg+xml");
 
