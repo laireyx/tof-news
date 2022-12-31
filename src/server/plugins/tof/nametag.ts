056269d52c926bbcc0e3d6e11e54f0f252d512eb
laireyx <laireyx@gmail.com>
2023-01-01 03:21:20 +0900

    data uri로 변경하여 embedding 가능하게 수정

--- src/server/plugins/tof/nametag.ts
+++ src/server/plugins/tof/nametag.ts
@@ -16,6 +16,28 @@ declare module "fastify" {
 export default fp(
   async function (fastify, opts) {
     const collection = fastify.mongo.db?.collection<LookupRecord>("lookup");
+    const dataUris: Map<string, string> = new Map();
+    const got = await import("got").then((module) => module.default);
+
+    async function urlToDataUri(url: string) {
+      let b64: string;
+
+      if (dataUris.has(url)) {
+        b64 = dataUris.get(url)!;
+      } else {
+        b64 = await got(url)
+          .then((resp) => resp.rawBody)
+          .then((buffer) => buffer.toString("base64"));
+
+        dataUris.set(url, b64);
+      }
+
+      return b64;
+    }
+
+    const avatarImg = await urlToDataUri(
+      "https://www.tof.news/img/nametag/Avatar_Fenrir.webp"
+    );
 
     fastify.decorate(
       "tofNametag",
@@ -31,16 +53,19 @@ export default fp(
         if (Date.now() < user.timestamp + +(env.LOOKUP_EXPIRE ?? "3600000"))
           fastify.tofLookupByUid(uid);
 
-        const weaponImages = user.data.weapons
-          .map(
-            (weapon, idx) =>
-              `<image x="${
+        const weaponImages = (
+          await Promise.all(
+            user.data.weapons.map(async (weapon, idx) => {
+              const b64 = await urlToDataUri(
+                `https://www.tof.news/img/weapon/${weapon.name}.webp`
+              );
+
+              return `<image x="${
                 192 + idx * 88
-              }" y="160" width="80" height="80" href="//www.tof.news/img/weapon/${
-                weapon.name
-              }.webp"/>`
+              }" y="160" width="80" height="80" href="data:image/webp;base64,${b64}"/>`;
+            })
           )
-          .join("");
+        ).join("");
 
         return {
           svg: `<svg width="480" height="320" viewBox="0 0 480 320" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -48,7 +73,7 @@ export default fp(
           <style>@font-face{font-family:NanumSquareRound;font-style:normal;font-weight:300;src:local("NanumSquareRoundL"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundL.eot),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundL.eot?#iefix)format("embedded-opentype"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundL.woff2)format("woff2"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundL.woff)format("woff"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundL.ttf)format("truetype")}@font-face{font-family:NanumSquareRound;font-style:normal;font-weight:400;src:local("NanumSquareRoundR"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundR.eot),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundR.eot?#iefix)format("embedded-opentype"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundR.woff2)format("woff2"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundR.woff)format("woff"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundR.ttf)format("truetype")}@font-face{font-family:NanumSquareRound;font-style:normal;font-weight:700;src:local("NanumSquareRoundB"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundB.eot),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundB.eot?#iefix)format("embedded-opentype"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundB.woff2)format("woff2"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundB.woff)format("woff"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundB.ttf)format("truetype")}@font-face{font-family:NanumSquareRound;font-style:normal;font-weight:800;src:local("NanumSquareRoundEB"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundEB.eot),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundEB.eot?#iefix)format("embedded-opentype"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundEB.woff2)format("woff2"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundEB.woff)format("woff"),url(//cdn.jsdelivr.net/gh/innks/NanumSquareRound@master/NanumSquareRoundEB.ttf)format("truetype")}@font-face{font-family:"IBM Plex Mono";font-style:normal;font-weight:400;font-display:swap;src:url(//fonts.gstatic.com/s/ibmplexmono/v15/-F63fjptAgt5VM-kVkqdyU8n1i8q1w.woff2)format("woff2");unicode-range:U+0000-00FF,U+0131,U+0152-0153,U+02BB-02BC,U+02C6,U+02DA,U+02DC,U+2000-206F,U+2074,U+20AC,U+2122,U+2191,U+2193,U+2212,U+2215,U+FEFF,U+FFFD}.info{font-family:NanumSquareRound,sans-serif;fill:#fff;text-shadow:1px 1px 2px #87ceeb}.uid{font-family:IBM Plex Mono,ui-monospace,monospace;font-size:12px;fill:#fff}</style>
           
           <path d="M2 34Q0 0 34 2h412q34-2 32 30v254q2 34-32 32H34q-34 2-32-32Z" fill="#030066" stroke="#fff" stroke-width="2"/>
-          <image y="64" width="192" height="192" href="//www.tof.news/img/nametag/Avatar_Fenrir.webp"/>
+          <image y="64" width="192" height="192" href="data:image/webp;base64,${avatarImg}"/>
           <text x="192" y="72" style="fill:#fff;font-family:NanumSquareRound,sans-serif;font-weight:600;font-size:32px;text-shadow:1px 1px 4px #87ceeb">
           ${user.name}
           </text>
