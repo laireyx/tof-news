cc11c85663cf36df183491b6af7619ad6233ebec
laireyx <laireyx@gmail.com>
2023-03-11 14:10:55 +0900

    Refresh both active and old user

--- src/server/plugins/tof/refresh.ts
+++ src/server/plugins/tof/refresh.ts
@@ -1,28 +1,40 @@
 import fp from "fastify-plugin";
 import { env } from "node:process";
 import { LookupRecord } from "../../tof/lookup";
+import type { Collection } from "mongodb";
 
 export default fp(
   async function (fastify, opts) {
-    const collection =
+    function refreshCollection(
+      collection: Collection<LookupRecord> | undefined
+    ) {
+      const cb = async function () {
+        await collection
+          ?.find()
+          .sort({ timestamp: 1 })
+          .limit(10)
+          .forEach((record) => {
+            fastify.tofLookupByUid(record.uid, record.server);
+          });
+
+        setTimeout(() => cb(), refreshInterval);
+      };
+
+      return cb;
+    }
+
+    const activeUsers =
       fastify.mongo.db?.collection<LookupRecord>("active-users");
+    const allUsers = fastify.mongo.db?.collection<LookupRecord>("lookup");
 
     const refreshInterval = +(env.REFRESH_INTERVAL ?? -1);
 
-    const refresh = async function () {
-      await collection
-        ?.find()
-        .sort({ timestamp: 1 })
-        .limit(10)
-        .forEach((record) => {
-          fastify.tofLookupByUid(record.uid, record.server);
-        });
-
-      setTimeout(() => refresh(), refreshInterval);
-    };
+    const refreshActive = refreshCollection(activeUsers);
+    const refreshAll = refreshCollection(allUsers);
 
     if (refreshInterval > 0) {
-      await refresh();
+      await refreshActive();
+      await refreshAll();
 
       console.log(
         `[@plugin/tof/refresh] User refresh plugin registered: ${refreshInterval}`
